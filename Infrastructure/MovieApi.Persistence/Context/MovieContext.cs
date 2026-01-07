d53368da6d9cdb607dc769ad43833462ff917090
Sinan Engin Yıldız <sinan.yildiz@sipay.com.tr>
2026-01-07 16:29:10 +0300

    initial commit

---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=MovieDb;Trusted_Connection=True;");
+        }
+
+    }
+}
